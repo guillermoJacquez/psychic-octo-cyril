9c91adb4282e5a9bd86e79272c5af640826c3973
gjacquez <gjacquez@shoplocal.com>
2014-08-20 14:20:50 -0500

    adding semi-colons

--- address.js
+++ address.js
@@ -1,12 +1,12 @@
 function address(){
 	
-	this.line1
-	this.line2
-	this.line3
+	this.line1;
+	this.line2;
+	this.line3;
 	
-	this.city
-	this.state
-	this.zipCode
+	this.city;
+	this.state;
+	this.zipCode;
 	
 	this.getFullAddress = function(){
 		return this.line1 + " " + this.line2;
